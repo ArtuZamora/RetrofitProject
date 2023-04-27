224bfb10011db8f2545eceaec15d71f4a9ce5708
Arturo Zamora <r2zamora11@gmail.com>
2023-04-26 23:55:50 -0600

    Added: Splash

---
+++ app/src/main/java/com/dsm/retrofitproject/activities/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.dsm.retrofitproject.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dsm.retrofitproject.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
